739e4bd76b4e7472442a4cefcd8db6c406d56da4
Toshiya Doi <doi.t.npdp@gmail.com>
2019-10-19 16:26:07 +0900

    Add 'home' page

---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <Welcome />
+      </v-flex>
+      <v-flex xs12>
+        <HowToPlay />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import Welcome from "~/components/Welcome.vue";
+import HowToPlay from "~/components/HowToPlay.vue";
+
+export default {
+  components: {
+    Welcome,
+    HowToPlay
+  }
+};
+</script>
