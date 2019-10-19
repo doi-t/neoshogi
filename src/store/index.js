9f54264999d348c42eaaedd04f1683f20b52319b
Toshiya Doi <doi.t.npdp@gmail.com>
2019-10-19 14:14:53 +0900

    Define authenticated state in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
 import JWTDecode from "jwt-decode";
 import cookieparser from "cookieparser";
 
+export const state = () => ({
+  authenticated: false
+});
+
+export const getters = {
+  IsAuthenticated: state => state.authenticated
+};
+
 export const actions = {
   nuxtServerInit({ commit }, { req }) {
     if (process.server && process.static) return;
@@ -19,5 +27,13 @@ export const actions = {
         email: decoded.email
       });
     }
+  },
+  async setAuthenticated({ commit }, authStatus) {
+    console.log("Updating status of auth...");
+    commit("setAuthenticated", authStatus);
   }
+};
+
+export const mutations = {
+  setAuthenticated: (state, authStatus) => (state.authenticated = authStatus)
 };
