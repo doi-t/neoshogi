713cd905e2aa173a7212a87122fd9c01656c6aca
Toshiya Doi <doi.t.npdp@gmail.com>
2019-10-20 23:48:26 +0900

    Verify ID token in middleware

--- src/middleware/authenticated.js
+++ src/middleware/authenticated.js
@@ -1,9 +1,32 @@
 // See https://nuxtjs.org/api/context/ to understand arguments
-export default ({ store, route, redirect }) => {
+export default ({ req, store, route, redirect }) => {
+  if (process.server) {
+    if (!req.headers.cookie) {
+      console.error("Couldnt' find a cookie in the request header.");
+      redirect("/");
+    }
+
+    var admin = require("firebase-admin");
+    const cookieparser = require("cookieparser");
+    const token = cookieparser.parse(req.headers.cookie).access_token;
+    admin
+      .auth()
+      .verifyIdToken(token)
+      .then(decodedToken => {
+        console.log("ID Token has been verified.");
+        store.dispatch("setVerifiedToken", decodedToken).catch(error => {
+          console.error(error);
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        redirect("/");
+      });
+  }
+
   const user = store.state.users.user;
   const blockedRoute = /\/(home|game|mypage|logout)\/*/g;
   const homeRoute = "/";
-
   if (!user && route.path.match(blockedRoute)) {
     redirect("/");
   }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,3 @@
-import JWTDecode from "jwt-decode";
-import cookieparser from "cookieparser";
-import admin from "firebase-admin";
-
 export const state = () => ({
   authenticated: false
 });
@@ -11,39 +7,16 @@ export const getters = {
 };
 
 export const actions = {
-  nuxtServerInit({ commit }, { req }) {
-    if (process.server && process.static) return;
-    if (!req.headers.cookie) return;
-
-    const parsed = cookieparser.parse(req.headers.cookie);
-    const accessTokenCookie = parsed.access_token;
-
-    if (!accessTokenCookie) return;
-
-    admin
-      .auth()
-      .verifyIdToken(accessTokenCookie)
-      .then(() => {
-        console.log("ID Token has been verified.");
-        return;
-      })
-      .catch(error => {
-        console.error(error);
-        return;
-      });
-
-    const decoded = JWTDecode(accessTokenCookie);
-
-    if (decoded) {
-      commit("users/SET_USER", {
-        uid: decoded.user_id,
-        email: decoded.email
-      });
-    }
+  async setVerifiedToken({ commit }, token) {
+    commit("users/SET_USER", {
+      uid: token.uid,
+      email: token.email
+    });
+    console.log("Set verified token.");
   },
   async setAuthenticated({ commit }, authStatus) {
-    console.log("Updating status of auth...");
     commit("setAuthenticated", authStatus);
+    console.log(`Updated status of auth as ${authStatus}.`);
   }
 };
 
