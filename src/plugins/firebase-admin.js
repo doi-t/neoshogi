87104687aaead207fe10202979af0b1d88349416
Toshiya Doi <doi.t.npdp@gmail.com>
2019-10-22 11:43:01 +0900

    Use admin.credential.applicationDefault() to avoid a credential file

--- src/plugins/firebase-admin.js
+++ src/plugins/firebase-admin.js
@@ -1,9 +1,8 @@
 if (process.server) {
   var admin = require("firebase-admin");
   if (!admin.apps.length) {
-    var serviceAccount = require("../../.firebase/firebase-adminsdk.json");
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.applicationDefault(),
       databaseURL: "https://doi-t-alpha.firebaseio.com"
     });
   }
