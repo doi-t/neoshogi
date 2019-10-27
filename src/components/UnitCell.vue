38f0105a0b50b07298064cdf750c104f287128bb
Toshiya Doi <doi.t.npdp@gmail.com>
2019-10-28 01:29:58 +0900

    Change UnitCell component color upon the cell condition

--- src/components/UnitCell.vue
+++ src/components/UnitCell.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card class="pa-2" outlined tile @click.native="updateCell()">
+    <v-card :class="cellColor" class="pa-2" outlined tile @click.native="updateCell()">
       row:{{ row }}, col:{{ col }}
       <br />
       {{ unitStatus }}
@@ -11,9 +11,23 @@
 <script>
 export default {
   props: ["row", "col"],
+  data: () => ({}),
   computed: {
+    cellColor() {
+      if (this.$store.state.db.gameStatus.cells[this.row][this.col].selected)
+        return "red";
+      if (this.$store.state.db.gameStatus.cells[this.row][this.col].marked)
+        return "green";
+      return "blue";
+    },
     unitStatus() {
       return this.$store.state.db.gameStatus.cells[this.row][this.col];
+    },
+    selected() {
+      return { active: this.$store.state.db.player.selected };
+    },
+    marked() {
+      return this.$store.state.db.player.marked;
     }
   },
   methods: {
