df6c09ba24371bd5afd89c4127b03ece280c83a4
Toshiya Doi <doi.t.npdp@gmail.com>
2019-11-04 00:50:39 +0900

    Tweak the game board style

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,16 +1,26 @@
 <template>
   <v-container>
     <v-row no-gutters>
+      <v-col cols="2">
+        <v-card>
+          <v-select v-model="selectedScale" :items="scales" label="Choose a game scale"></v-select>
+        </v-card>
+      </v-col>
+      <v-col cols="10">
+        <v-card>
+          <v-card-actions>
+            <v-btn @click="initiateGame()">initiate</v-btn>
+            <v-btn @click="resetAction()">reset</v-btn>
+            <v-btn @click="moveUnit()">move a unit</v-btn>
+            <v-btn color="#E53935">Give Up</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
       <v-col cols="12">
         <v-card>
-          <v-card-title>>GameBoard</v-card-title>
-          <v-btn @click="initiateGame()">initiate</v-btn>
-          <v-btn @click="resetAction()">reset</v-btn>
-          <v-btn @click="moveUnit()">move a unit</v-btn>
-          <v-select v-model="selectedScale" :items="scales" label="Choose a game scale"></v-select>
           <v-container>
-            <div class="text-center d-flex justify-center align-center xs-12 flex-wrap">
-              <v-card>
+            <div class="text-center justify-center align-center d-flex">
+              <v-card class="pa-2">
                 <v-row v-for="(row, rowIndex) in cells" :key="rowIndex" no-gutters>
                   <v-col v-for="(col, colIndex) in row" :key="colIndex">
                     <Cell :row="rowIndex" :col="colIndex" />
